8031ccd7d7e1a4b45932707e6f7f9d15199f0dca
デスクトップPC <debtgx103@gmail.com>
2021-06-15 17:39:38 +0900

    ひとまずこっみっと

---
+++ lib/ptest.dart
@@ -0,0 +1,10 @@
+library ptest;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
+class MyHello {
+  void test(int x){print('hello');
+}
